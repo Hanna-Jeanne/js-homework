a8fe73f98bb6fda76e52420e7c592bbb46b36b85
Hanna-Jeanne <janghanna423@gmail.com>
2024-11-18 17:59:27 +0900

    로그인 과제 피드백

--- mission1/naver_login/js/main.js
+++ mission1/naver_login/js/main.js
@@ -7,72 +7,90 @@
 
 */
 
+const user = {
+  id: "asd@naver.com",
+  pw: "spdlqj123!@",
+};
+const emailInput = document.querySelector("#userEmail");
+const pwInput = document.querySelector("#userPassword");
+const loginButton = document.querySelector(".btn-login");
+
+let emailPass = false;
+let pwPass = false;
+
+// @ 기호 포함, .포함 이후 2글자 이상
 function emailReg(text) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   return re.test(String(text).toLowerCase());
 }
-
+// 최소 6글자 이상, 0~9숫자 1개 이상 포함, 특수기호 1개 이상 포함
 function pwReg(text) {
   const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
   return re.test(String(text).toLowerCase());
 }
 
-const user = {
-  id: "asd@naver.com",
-  pw: "spdlqj123!@",
-};
+function handleCheckEmail() {
+  const value = this.value;
 
-/* -------------- */
+  console.log(value);
 
-const emailInput = document.querySelector(".user-email-input");
-const passwordInput = document.querySelector(".user-password-input");
-
-const errorMessage = document.querySelectorAll(".error-message");
-const emailErrorMessage = errorMessage[0];
-const passwordErrorMessage = errorMessage[1];
-
-function handleInput() {
-  if (this.value === user.id) {
-    console.log("통과");
+  if (emailReg(value)) {
+    this.classList.remove("is--invalid");
+    emailPass = true;
   } else {
-    this.classlist;
+    this.classList.add("is--invalid");
+    emailPass = false;
   }
 }
 
-emailInput.addEventListener("input", handleInput);
+function handleCheckPw() {
+  const value = this.value;
 
-/* -------------- */
-
-//
-const btnLogin = document.querySelector(".btn-login");
-
-function nextPage() {
-  if (emailInput.value === user.id && passwordInput.value === user.pw) {
-    window.location.href = "/mission1/naver_login/welcome.html";
+  if (pwReg(value)) {
+    this.classList.remove("is--invalid");
+    pwPass = true;
+  } else {
+    this.classList.add("is--invalid");
+    pwPass = false;
   }
 }
 
-btnLogin.addEventListener("click", nextPage);
+function handleLogin(e) {
+  e.preventDefault();
 
-//
-// try {
-//   function nextPage() {
-//     if (emailInput[value] === user[id] && passwordInput[value] === user[pw]) {
-//       window.location.href = "/mission1/naver_login/welcome.html";
-//     }
+  if (emailPass && pwPass) {
+    try {
+      const id = emailInput.value;
+      const pw = pwInput.value;
+      const getUserId = user.id; // 1s
+      const getUserPw = user.pw; // 1s
 
-//     if (emailInput[value] !== user[id] || passwordInput[value] !== user[pw]) {
-//       throw new ReferenceError("해당 키 값이 존재하지 않습니다.");
-//     }
-//   }
+      if (id === getUserId && pw === getUserPw) {
+        console.log("로그인 성공!");
+      } else {
+        throw new Error("...");
+      }
+    } catch {
+      alert("아이디 또는 비밀번호가 일치하지 않습니다.");
+    }
+  }
 
-//   btnLogin.addEventListener("click", nextPage);
-// } catch {
-//   console.log("error!!");
+  // console.log(id, pw);
+}
 
-//   document.body.innerHTML = "뭔가 잘못됐어!!";
-// }
-/* ---------------------------------------------------------------------------------------- */
-/*                 죄송합니다. 아무것도 되는 게 없지만 몇 시간에 걸쳐 뭔가를 한 결과입니다...                */
-/* ---------------------------------------------------------------------------------------- */
+function debounce(callback, limit = 500) {
+  let timeout;
+
+  return function (e) {
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      callback.call(this, e);
+    }, limit);
+  };
+}
+
+emailInput.addEventListener("input", debounce(handleCheckEmail, 1000));
+pwInput.addEventListener("input", handleCheckPw);
+loginButton.addEventListener("click", handleLogin);
