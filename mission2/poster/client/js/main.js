1e145c0b85e14ef30af8776ecac74587436d60e2
Hanna-Jeanne <janghanna423@gmail.com>
2024-11-24 01:47:37 +0900

    엘리멘탈 과제 - 최소 요건 충족

--- mission2/poster/client/js/main.js
+++ mission2/poster/client/js/main.js
@@ -1,3 +1,4 @@
+import { data } from "./data.js";
 
 /* 
 
@@ -9,16 +10,57 @@
 
 */
 
+(() => {
+  const body = document.querySelector("body");
 
+  const nickName = document.querySelector(".nickName");
 
+  const visualImg = document.querySelector(".visual > div > img");
 
+  const nav = document.querySelector(".nav");
 
+  /* ------------ setBgColor ------------ */
+  function setBgColor(data) {
+    const [colorA, colorB = "#000"] = data.color;
 
+    body.style.background = `linear-gradient(to bottom, ${colorA},${colorB})`;
+  }
 
+  /* ------------ setImage ------------ */
+  function setImage(data) {
+    visualImg.src = `./assets/${data.name}.jpeg`;
+    visualImg.alt = data.alt;
+  }
 
+  /* ------------ setNameText ------------ */
+  function setNameText(data) {
+    nickName.textContent = data.name;
+  }
 
+  /* ------------ handler ------------ */
+  function handler(e) {
+    e.preventDefault();
 
+    const target = e.target.closest("li");
+    const thumbNails = this.querySelectorAll("ul > li");
 
+    if (!target) return;
 
+    const index = target.dataset.index;
 
+    thumbNails.forEach((li) => {
+      li.classList.remove("is-active");
+    });
 
+    target.classList.add("is-active");
+
+    setBgColor(data[index - 1]);
+
+    setImage(data[index - 1]);
+
+    setNameText(data[index - 1]);
+  }
+
+  /* ------------ nav.clilkEvent ------------ */
+  nav.addEventListener("click", handler);
+})();
